dec84e2527b061f46916eb431deaa0342ca6d945
Ronit <darjironit1@gmail.com>
2024-12-27 20:25:23 +0530

    pr1

---
+++ lec2/index.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById(`data`).innerHTML = `
+ 
+    <tr>
+        <td>name</td>
+        <td>age</td>
+        <td>email</td>
+    </tr>
+     <tr>
+        <td>dhanji</td>
+        <td>56</td>
+        <td>dhaji@gmail.com</td>
+    </tr>
+       <tr>
+        <td>dhanji</td>
+        <td>56</td>
+        <td>dhaji@gmail.com</td>
+    </tr>
+
+
+`
+
+document.getElementById(`list`).innerHTML = `
+                    <li>playing</li>
+                    <li>running</li>
+                    <li>working</li>
+`
